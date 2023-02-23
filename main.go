3b30e2f56098d75bf4131fb12f5aa2686b781b5f
Eugene Ryoo <eugryooene@gmail.com>
2023-02-23 14:30:32 -0500

    better artist page + all rotser landing

--- main.go
+++ main.go
@@ -26,23 +26,48 @@ func new_home(c *gin.Context) {
 
 }
 
-func artist(c *gin.Context) {
-
-	// get the specified artist
+// return all artists
+func get_artists() []Artist {
 	artists := []Artist{}
 
+	// LEG
 	sharkies_2 := Work{"Sharkies 2", "LEG", "2019", "/assets/album_art/leg/sharkies2.png"}
 	johnstown := Work{"Johnstown", "LEG", "2020", "/assets/album_art/leg/johnstown.png"}
 	leg3 := Work{"LEG 3", "LEG", "2023", "/assets/album_art/leg/leg3.png"}
 
-	works := []Work{sharkies_2, johnstown, leg3}
+	leg_works := []Work{sharkies_2, johnstown, leg3}
+	leg_band := Artist{1, "LEG", "/assets/artists/leggy.jpg", leg_works, "LEG produces one of a kind musical adventures that explore the human condition. By mixing progressive electronic music and hip hop, LEG creates a unique textural blend of sounds that brings listeners to surreal re-imaginings of not so iconic locations."}
 
-	artists = append(artists, Artist{1, "LEG", "/assets/artists/leggy.jpg", works, "LEG produces one of a kind musical adventures that explore the human condition.  By mixing progressive electronic music and hip hop, LEG creates a unique textural blend of sounds that brings listeners to surreal re-imaginings of not so iconic locations.  "})
+	// TWB
+	goblinrock2 := Work{"Goblin Rock II", "the willard building", "2022", "https://f4.bcbits.com/img/a1430038869_16.jpg"}
+	years99 := Work{"99 years of contract hell", "the willard building", "2023", "/assets/album_art/twb/coveridea1.png"}
+	poppy := Work{"Poppy", "the willard building", "2023", "/assets/album_art/twb/lilcat.jpg"}
+
+	twb_works := []Work{goblinrock2, years99, poppy}
+	twb_band := Artist{2, "the willard building", "/assets/artists/twb artist.jpg", twb_works, "it is unclear if the willard building is a band or a building but somehow eugene, ryan, grant, derek, and andrew are involved."}
+
+	artists = append(artists, leg_band, twb_band)
+
+	return artists
+}
+
+func all_artists(c *gin.Context) {
+	artists := get_artists()
+
+	c.HTML(http.StatusOK, "all_artists.html", gin.H{
+		"artists": artists,
+	})
+
+}
+
+func artist(c *gin.Context) {
 
 	pk := c.Param("id")
 	pk_int, err := strconv.Atoi(pk)
 
 	println(err)
+
+	artists := get_artists()
 
 	if err == nil {
 		println("pk2", pk_int)
@@ -62,7 +87,8 @@ func main() {
 	router.Static("/assets", "./assets")
 	// routes
 	router.GET("/home/", new_home)
-	router.GET("/artist/:id/", artist)
+	router.GET("/artists/", all_artists)
+	router.GET("/artists/:id/", artist)
 	router.Run("localhost:8080")
 }
 
