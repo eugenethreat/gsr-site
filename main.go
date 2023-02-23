3f02683cf762b3c14a16ac118dfcc47f6b312961
Eugene Ryoo <eugryooene@gmail.com>
2023-02-23 12:15:50 -0500

    new home. templatize...

--- main.go
+++ main.go
@@ -6,8 +6,37 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// https://www.allhandsontech.com/programming/golang/how-to-use-sqlite-with-go/
+func new_home(c *gin.Context) {
 
+	// generate a list of works
+	// https://www.allhandsontech.com/programming/golang/how-to-use-sqlite-with-go/
+	goblinrock2 := Work{"Goblin Rock II", "the willard building", "2022", "https://f4.bcbits.com/img/a1430038869_16.jpg"}
+	gromulet := Work{"Destiny's Gromulet", "mango safari", "2022", "https://f4.bcbits.com/img/a3203506842_16.jpg"}
+	fuckaround := Work{"fuck around and find out", "H2OhNo!", "2022", "https://f4.bcbits.com/img/a2613250794_16.jpg"}
+
+	works := []Work{goblinrock2, gromulet}
+
+	// works = append(works, Work{"LEG 3", "LEG", "2022", ""})
+	works = append(works, fuckaround)
+
+	c.HTML(http.StatusOK, "base.html", gin.H{
+		"work_test": works,
+	})
+
+}
+
+func main() {
+	router := gin.Default()
+	// load html
+	router.LoadHTMLGlob("templates/*/*")
+	// load static
+	router.Static("/assets", "./assets")
+	// routes
+	router.GET("/home/", new_home)
+	router.Run("localhost:8080")
+}
+
+// old
 func home(c *gin.Context) {
 
 	// generate a list of works
@@ -25,15 +54,4 @@ func home(c *gin.Context) {
 		"work_test": works,
 	})
 
-}
-
-func main() {
-	router := gin.Default()
-	// load html
-	router.LoadHTMLGlob("templates/*")
-	// load static
-	router.Static("/assets", "./assets")
-	// routes
-	router.GET("/home", home)
-	router.Run("localhost:8080")
 }
